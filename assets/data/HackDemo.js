7dbf1d6ebc9624598f86bdab1eda7b7b5ddf0a52
Eric Eldridge <erice224@gmail.com>
2019-11-03 06:02:51 -0500

    Pics/Works

---
+++ assets/data/HackDemo.js
@@ -0,0 +1,76 @@
+class Product{
+    constructor(name, price){
+    this.name = name;
+    this. price = price;
+    }
+}
+class Package{
+    constructor(p1, p2, p3, p4, p5){
+        Package = [p1,p2,p3,p4,p5];
+    }
+}
+
+// var arr = api();
+
+// function api(){
+//     fetch('https://gateway-staging.ncrcloud.com/transaction-document/transaction-documents/20191102-564452-10100006-0', {
+//      method: 'POST',
+//      headers: {
+//        'accept': 'application/json',
+//        'Content-Type': 'application/json',
+//        'Authorization': 'acct:root@hack_foodhub:~+J<96!]}u',
+//        'nep-application-key': '8a0384356ddb119e016e2ab8ebf10090',
+//        'nep-organization': 'silver-merchant-552518',
+//        'nep-service-version': '2:1'
+//      },
+//     }).then(response => response.json()).then(Data =>{
+//         return (response.json());
+//     });//    .catch((error) => {
+    
+    //      console.error(error);
+    //    });
+    //}
+
+
+
+var prods= [
+    eggs = new Product("Eggs", 3.50),
+    milk = new Product("Milk", 2.75),
+    bananas = new Product("Bananas", 1.90),
+     bread = new Product("Bread", 1.99),
+     brownie = new Product("Brownie", 1.00),
+     vIceCream = new Product("Vanilla Ice cream", 5.90),
+     cSyrup = new Product("Chocolate Syrup", 1.50),
+     wCream = new Product("Whipped Cream", 3.99),
+     cherries = new Product("Cherries", 1.00),
+     //brownieSunday = new Package(brownie, vIceCream, cSyrup, wCream, cherries)
+
+];
+
+module.exports.prods;
+// The fetch is throwing right away, after many hours of troubleshooting,
+// we are ignoring it and hardcoding for now.
+
+
+
+
+// // The fetch is throwing right away, after many hours of troubleshooting,
+// // we are ignoring it and hardcoding for now.
+
+// function api(){
+// fetch('https://gateway-staging.ncrcloud.com/transaction-document/transaction-documents/20191102-564452-10100006-0', {
+//  method: 'POST',
+//  headers: {
+//    'Accept': 'application/json',
+//    'Content-Type': 'application/json',
+//    'Authorization': 'Basic YWNjdDpyb290QGhhY2tfZm9vZGh1Yjp+K0o8OTYhXX11',
+//    'nep-application-key': '8a0384356ddb119e016e2ab8ebf10090',
+//    'nep-organization': 'silver-merchant-552518',
+//    'nep-service-version': '2:1'
+//  },
+// }).then(response => response.json())
+
+//    .catch((error) => {
+
+//      console.error(error);
+//    });
--- assets/data/demo.js
+++ assets/data/demo.js
@@ -1,9 +1,11 @@
+import prods from './HackDemo';
+
 module.exports = [
 	{
 		id: 1,
-		name: 'Leanne Graham',
+		name: "Eggs",
 		status: 'Online',
-		match: '78',
+		match: "$3.00",
 		description:
 			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
 		message:
@@ -12,8 +14,8 @@ module.exports = [
 	},
 	{
 		id: 2,
-		name: 'Clementine Bauch',
-		match: '93',
+		name: 'Milk',
+		match: '$2.30',
 		description:
 			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
 		status: 'Offline',
@@ -22,8 +24,8 @@ module.exports = [
 	},
 	{
 		id: 3,
-		name: 'Ervin Howell',
-		match: '45',
+		name: 'Bread',
+		match: '$1.60',
 		description:
 			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
 		status: 'Offline',
@@ -33,8 +35,8 @@ module.exports = [
 	},
 	{
 		id: 4,
-		name: 'John Lebsack',
-		match: '88',
+		name: 'Bananas',
+		match: '$0.73',
 		description:
 			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
 		status: 'Online',
@@ -43,73 +45,73 @@ module.exports = [
 	},
 	{
 		id: 5,
-		name: 'James Dietrich',
-		match: '76',
+		name: 'Brownie Sundae',
+		match: '$7.32',
 		description:
 			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
 		status: 'Offline',
 		message: "It's not who I am underneath but what I do that defines me.",
 		image: require('../images/05.jpg')
 	},
-	{
-		id: 6,
-		name: 'Patricia Schulist',
-		match: '95',
-		description:
-			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
-		status: 'Online',
-		message:
-			'You have nothing, nothing to threaten me with. Nothing to do with all your strength.',
-		image: require('../images/06.jpg')
-	},
-	{
-		id: 7,
-		name: 'Chelsey Weissnat',
-		match: '67',
-		description:
-			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
-		status: 'Online',
-		message:
-			"Never start with the head. The victim gets all fuzzy. He can't feel the next... See?",
-		image: require('../images/07.jpg')
-	},
-	{
-		id: 8,
-		name: 'Nicky Runol',
-		match: '85',
-		description:
-			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
-		age: '27',
-		location: 'Irvine, CA',
-		info1: 'Straight, Single, 5"10',
-		info2: 'Tea Totaller, Loves Photography & Travel',
-		info3: 'Beaches, Mountain, Cafe, Movies',
-		info4: 'Last seen: 23h ago',
-		status: 'Online',
-		message:
-			"And as for the television's so-called plan, Batman has no jurisdiction.",
-		image: require('../images/08.jpg')
-	},
-	{
-		id: 9,
-		name: 'Glenna Reichert',
-		match: '74',
-		description:
-			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
-		status: 'Online',
-		message:
-			'This is what happens when an unstoppable force meets an immovable object.',
-		image: require('../images/09.jpg')
-	},
-	{
-		id: 10,
-		name: 'Kurtis DuBuque',
-		match: '98',
-		description:
-			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
-		status: 'Offline',
-		message:
-			'You want order in Gotham. Batman must take off his mask and turn himself in.',
-		image: require('../images/10.jpg')
-	}
+	// {
+	// 	id: 6,
+	// 	name: 'Patricia Schulist',
+	// 	match: '95',
+	// 	description:
+	// 		'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
+	// 	status: 'Online',
+	// 	message:
+	// 		'You have nothing, nothing to threaten me with. Nothing to do with all your strength.',
+	// 	image: require('../images/06.jpg')
+	// },
+	// {
+	// 	id: 7,
+	// 	name: 'Chelsey Weissnat',
+	// 	match: '67',
+	// 	description:
+	// 		'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
+	// 	status: 'Online',
+	// 	message:
+	// 		"Never start with the head. The victim gets all fuzzy. He can't feel the next... See?",
+	// 	image: require('../images/07.jpg')
+	// },
+	// {
+	// 	id: 8,
+	// 	name: 'Nicky Runol',
+	// 	match: '85',
+	// 	description:
+	// 		'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
+	// 	age: '27',
+	// 	location: 'Irvine, CA',
+	// 	info1: 'Straight, Single, 5"10',
+	// 	info2: 'Tea Totaller, Loves Photography & Travel',
+	// 	info3: 'Beaches, Mountain, Cafe, Movies',
+	// 	info4: 'Last seen: 23h ago',
+	// 	status: 'Online',
+	// 	message:
+	// 		"And as for the television's so-called plan, Batman has no jurisdiction.",
+	// 	image: require('../images/08.jpg')
+	// },
+	// {
+	// 	id: 9,
+	// 	name: 'Glenna Reichert',
+	// 	match: '74',
+	// 	description:
+	// 		'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
+	// 	status: 'Online',
+	// 	message:
+	// 		'This is what happens when an unstoppable force meets an immovable object.',
+	// 	image: require('../images/09.jpg')
+	// },
+	// {
+	// 	id: 10,
+	// 	name: 'Kurtis DuBuque',
+	// 	match: '98',
+	// 	description:
+	// 		'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
+	// 	status: 'Offline',
+	// 	message:
+	// 		'You want order in Gotham. Batman must take off his mask and turn himself in.',
+	// 	image: require('../images/10.jpg')
+	// }
 ];
